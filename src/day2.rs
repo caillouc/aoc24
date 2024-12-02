40950186adeccd47017075a0b30bb971e886a943
Pierre Colson <pierre.online@clsn.fr>
2024-12-02 21:55:39 +0100

    day2

---
+++ src/day2.rs
@@ -0,0 +1,84 @@
+pub fn solve(data: String) {
+    println!("Part one : {}", solve1(&data));
+    println!("Part two : {}", solve2(&data));
+
+}
+
+pub fn parse(data: & String) -> Vec<Vec<i32>> {
+    data.lines()
+        .map(|l| 
+            l.split(' ')
+             .map(|i| 
+                i.parse::<i32>()
+                 .unwrap())
+             .collect())
+        .collect()
+} 
+
+
+
+fn is_safe(level: &[i32], resilient: bool) -> bool {
+    let increase = level.get(0) < level.get(1);
+    let mut previous = level.get(0).unwrap();
+    let mut first_fault = false;
+    for i in &level[1..] {
+        if !increase && i > previous || 
+            increase && i < previous ||
+            (i - previous).abs() < 1 ||
+            (i - previous).abs() > 3 {
+            if !resilient {
+                return false
+            } else if first_fault {
+                return is_safe(&level[1..], false) || 
+                       is_safe(&[&[level[0]], &level[2..]].concat(), false);
+            } else {
+                first_fault = true;
+                continue;
+            }
+        }     
+        previous = i;
+    }
+    true
+}
+
+
+fn solve1(data: &String) -> usize {
+    let levels = parse(data);
+    levels.iter().map(|l| is_safe(l, false)).filter(|b| *b).count()
+}
+
+fn solve2(data: &String) -> usize {
+    let levels = parse(data);
+    levels.iter().map(|l| is_safe(l, true)).filter(|b| *b).count()
+}
+
+#[test]
+fn part_one() {
+    let res = solve1(&String::from("7 6 4 2 1
+1 2 7 8 9
+9 7 6 2 1
+1 3 2 4 5
+8 6 4 4 1
+1 3 6 7 9
+"));
+    assert_eq!(2, res);
+}
+
+#[test]
+fn part_two() {
+    let res = solve2(&String::from("7 6 4 2 1
+1 2 7 8 9
+9 7 6 2 1
+1 3 2 4 5
+8 6 4 4 1
+1 3 6 7 9
+"));
+    assert_eq!(4, res);
+}
+
+
+#[test]
+fn custom_test() {
+    let res = solve2(&String::from("5 8 4 3 2 1"));
+    assert_eq!(1, res)
+}
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,24 @@
 use std::fs;
+use std::env;
 
 mod day1;
+mod day2;
 
 
 fn main() {
-    let data = fs::read_to_string("data/day1.txt").unwrap();
-    day1::solve(data);
+    let args: Vec<String> = env::args().collect();
+    
+    if args.len() != 2 {
+        panic!("You must provide one argument (the day number");
+    }
+
+    let day: i32 = args[1].parse().expect(format!("Cannot parse day {}", args[1]).as_str());
+    let file_path = format!("data/day{day}.txt");
+    let data = fs::read_to_string(&file_path).expect(format!("Cannot read file {}", file_path).as_str());
+
+    match day {
+        1 => day1::solve(data),    
+        2 => day2::solve(data),    
+        day => println!("Invalid day {}", day)
+    }
 }
