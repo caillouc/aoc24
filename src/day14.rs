d379da0aed0273c55fc5ee50be93de1b801910f4
Pierre Colson <pierre.online@clsn.fr>
2024-12-15 19:31:39 +0100

    day14

---
+++ src/day14.rs
@@ -0,0 +1,113 @@
+use std::{collections::HashSet, thread::sleep, time::Duration};
+
+use regex::Regex;
+
+use crate::utils::Position;
+
+pub fn solve(data: String) {
+    let robots = parse(data);
+    println!("Part one : {}", solve1(&robots));
+    println!("Part two : {}", solve2(&robots));
+}
+
+#[derive(Debug)]
+struct Robot {
+    start_position: Position,
+    velocity: Position,
+}
+
+impl Robot {
+    fn evolve(&self, second: i32) -> Position {
+        let max_x = 101;
+        let max_y = 103;
+        Position::from(
+            (self.start_position.x() + second * self.velocity.x()).rem_euclid(max_x),
+            (self.start_position.y() + second * self.velocity.y()).rem_euclid(max_y),
+        )
+    }
+}
+
+fn parse(data: String) -> Vec<Robot> {
+    let mut robots = vec![];
+    for l in data.lines() {
+        let re = Regex::new(r"p=([0-9]*),([0-9]*) v=(-?[0-9]*),(-?[0-9]*)").unwrap();
+        let cap = re.captures(l).unwrap();
+        robots.push(Robot {
+            start_position: Position::from(cap[1].parse().unwrap(), cap[2].parse().unwrap()),
+            velocity: Position::from(cap[3].parse().unwrap(), cap[4].parse().unwrap()),
+        });
+    }
+    robots
+}
+
+fn solve1(robots: &Vec<Robot>) -> usize {
+    let mut res_positions = vec![];
+    for r in robots {
+        res_positions.push(r.evolve(100));
+    }
+    let mut count = 1;
+    count *= res_positions
+        .iter()
+        .filter(|p| p.y() < 51 && p.x() < 50)
+        .count();
+    count *= res_positions
+        .iter()
+        .filter(|p| p.y() > 51 && p.x() < 50)
+        .count();
+    count *= res_positions
+        .iter()
+        .filter(|p| p.y() > 51 && p.x() > 50)
+        .count();
+    count *= res_positions
+        .iter()
+        .filter(|p| p.y() < 51 && p.x() > 50)
+        .count();
+    count
+}
+
+fn solve2(robots: &Vec<Robot>) -> i32 {
+    let mut second = 1;
+    loop {
+        let mut res_positions = HashSet::new();
+        for r in robots {
+            res_positions.insert(r.evolve(second));
+        }
+        if res_positions.len() == robots.len() {
+            println!("Second {second}");
+            for y in 0..103 {
+                for x in 0..101 {
+                    if res_positions.contains(&Position::from(x, y)) {
+                        print!("X");
+                    } else {
+                        print!(" ");
+                    }   
+                }
+                println!();
+            }
+            sleep(Duration::from_secs(5));
+        }
+        second += 1;
+    }
+}
+//                     X             XX                                 X                              X
+
+#[test]
+fn part_one() {
+    let robots = parse(String::from(
+        "\
+p=0,4 v=3,-3
+p=6,3 v=-1,-3
+p=10,3 v=-1,2
+p=2,0 v=2,-1
+p=0,0 v=1,3
+p=3,0 v=-2,-2
+p=7,6 v=-1,-3
+p=3,0 v=-1,-2
+p=9,3 v=2,3
+p=7,3 v=-1,2
+p=2,4 v=2,-3
+p=9,5 v=-3,-3
+",
+    ));
+    assert_eq!(12, solve1(&robots));
+}
--- src/main.rs
+++ src/main.rs
@@ -16,6 +16,7 @@ mod day10;
 mod day11;
 mod day12;
 mod day13;
+mod day14;
 
 
 fn main() {
@@ -43,6 +44,7 @@ fn main() {
         11 => day11::solve(data),
         12 => day12::solve(data),
         13 => day13::solve(data),
+        14 => day14::solve(data),
         day => println!("Invalid day {}", day)
     }
 }
